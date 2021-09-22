dbc2acb6deca04a9e9aa32915220363134412e34
RaphaelCharousset <raphael.charousset@gmail.com>
2021-09-22 22:04:12 +0200

    merge feature_front/header

--- FRONT/src/components/Header/NavBarConnected/NavBarConnected.jsx
+++ FRONT/src/components/Header/NavBarConnected/NavBarConnected.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import home from '../../assets/navbar/location.png';
-import board from '../../assets/navbar/tablet.png';
-import profile from '../../assets/navbar/user.svg';
-import logout from '../../assets/navbar/log-out.svg';
+import home from '../../../assets/navbar/location.png';
+import board from '../../../assets/navbar/tablet.png';
+import profile from '../../../assets/navbar/user.svg';
+import logout from '../../../assets/navbar/log-out.svg';
 
 import './NavBarConnected.scss';
 
