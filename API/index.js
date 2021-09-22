f4694545999b786b2b2013f7a58a1fb6826092d0
collet julien <collet.jln@gmail.com>
2021-09-22 10:52:56 +0200

    init active record/router

--- API/index.js
+++ API/index.js
@@ -1,1 +1,15 @@
-// init
+require('dotenv').config();
+const express = require('express');
+const route = require('./app/router');
+
+const app = express();
+
+const port = process.env.PORT || 3500;
+
+app.use(route);
+
+
+
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
