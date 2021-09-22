5ab56b1cda1e9c989b081ea6f77c42e9122984e7
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-09-22 23:18:22 +0200

    :construction: more CSS but not done, add Link without reducer

--- FRONT/src/components/MapBalade/RideInformations/Profile/index.jsx
+++ FRONT/src/components/MapBalade/RideInformations/Profile/index.jsx
@@ -1,37 +1,39 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+// import { Link } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
 import './profile.scss';
 
 // import img
-import user from '../../../../assets/img/profile-simulation/user.jpg'; // assets/img/profile-simulation/user.jpg
+import user from '../../../../assets/img/profile-simulation/user.jpg';
 import dog from '../../../../assets/img/profile-simulation/dog-one.jpg';
 
 // import icon
 import race from '../../../../assets/img/profile-simulation/race.svg';
 import sociable from '../../../../assets/img/profile-simulation/sociable.svg';
 
-const Profile = () => (
-  <div className="profile">
+const Profile = () => {
+//   const id = useSlector((state) => state.reducer.id);
+  const a = 1;
+  return (
+    <div className="profile">
 
-    <div className="profile__picture">
-
-      <div className="profile__picture__user">
+      <div className="profile__user">
         <img src={user} alt="user picture" /> {/* mettre en alt le nom du user ? */}
-        <h3 className="profile__picture__user__name">Noémie</h3> {/* mettre dynamiquement les noms */}
+        <h3 className="profile__user__name">Noémie</h3> {/* mettre dynamiquement les noms */}
       </div>
 
-      <div className="profile__picture__dog">
-
-        <img src={dog} alt="dog picture" /> {/* mettre en alt nom du chien ? */}
-
-        <div className="icon"><img src={race} alt="dog icon" /></div>
-        <span>Race</span>
-        <div className="icon"><img src={sociable} alt="happy icon" /></div>
-        <span>Comportement</span>
-
+      <div className="profile__dog">
+        <img src={dog} alt="dog picture" /> {/* mettre en alt nom du chien */}
+        <p className="profile__dog__icon"><img src={race} alt="dog icon" />Race</p>{/* mettre en alt la race du chien ? */}
+        <p className="profile__dog__icon"><img src={sociable} alt="happy icon" />Comportement</p> {/* mettre en alt le comportement du chien ? */}
       </div>
+
+      {/* <Link to={`/profile/${id}`} className="profile_see">Voir le profil</Link> */}
     </div>
-  </div>
-);
+  );
+};
 
 export default Profile;
--- FRONT/src/components/MapBalade/RideInformations/index.jsx
+++ FRONT/src/components/MapBalade/RideInformations/index.jsx
@@ -1,24 +1,22 @@
-import React from "react";
-import {Popup} from "react-leaflet";
-import Ride from "./Ride/index";
-import Profile from "./Profile/index";
+import React from 'react';
+import { Popup } from 'react-leaflet';
+import Ride from './Ride/index';
+import Profile from './Profile/index';
 
-//import images
-import ride from "../../../assets/img/info-ride/dog-walk.svg";
+// import images
+import ride from '../../../assets/img/info-ride/dog-walk.svg';
 
-//import css
-import "./ride-informations.scss";
+// import css
+import './ride-informations.scss';
 
-const RideInformations = () => {
-    return (
-        <Popup className="ride-informations">
-            <h1><img className="icon" src={ride} alt="clock" />Nom de la balade</h1>
-            <div className="ride-informations__all">
-                <Ride />
-                <Profile />
-            </div>
-        </Popup>
-    )
-};
+const RideInformations = () => (
+  <Popup className="ride-informations">
+    <h1><img className="icon" src={ride} alt="clock" />Nom de la balade</h1>
+    <div className="ride-informations__all">
+      <Ride />
+      <Profile />
+    </div>
+  </Popup>
+);
 
 export default RideInformations;
