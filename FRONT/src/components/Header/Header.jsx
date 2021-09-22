1625e1b35a61295279ba76c4c2106099ddfa2625
RaphaelCharousset <raphael.charousset@gmail.com>
2021-09-22 22:04:12 +0200

    merge feature_front/header

--- FRONT/src/components/Header/Header.jsx
+++ FRONT/src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import NavBarConnected from '../NavBarConnected/NavBarConnected';
-import NavBarDisconnected from '../NavBarDisconnected/NavBarDisconnected';
+import NavBarConnected from './NavBarConnected/NavBarConnected';
+import NavBarDisconnected from './NavBarDisconnected/NavBarDisconnected';
 
 import './Header.scss';
 
