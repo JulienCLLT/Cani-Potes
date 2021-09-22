a27409496ed93888395f148e7a56f01735c89ad7
RaphaelCharousset <raphael.charousset@gmail.com>
2021-09-22 21:40:27 +0200

    adding basic router in App.jsx

--- FRONT/src/components/App/App.jsx
+++ FRONT/src/components/App/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Header from '../Header/Header';
 import MapBalade from '../MapBalade';
 import './App.scss';
+import Connection from '../Connection/Connection';
+import CreateRide from '../CreateRide/CreateRide';
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <Header />
-        <MapBalade />
-      </main>
+      <Switch>
+        <Route exact path="/">
+          <main>
+            <Connection />
+          </main>
+        </Route>
+        <Route exact path="/home">
+          <Header />
+          <main>
+            <MapBalade />
+          </main>
+        </Route>
+        <Route exact path="/ride/create">
+          <Header />
+          <main>
+            <CreateRide />
+          </main>
+        </Route>
+        <Route>
+          <main>404 : t'es tombé sur un os !</main>
+        </Route>
+      </Switch>
     </div>
   );
 }
