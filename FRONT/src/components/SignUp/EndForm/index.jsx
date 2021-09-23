8843dc7052ab286fdd6aeccb1b9dd958dbbd768f
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-09-23 21:57:48 +0200

    :construction: timeline out, css almost finished for user part

--- FRONT/src/components/SignUp/EndForm/index.jsx
+++ FRONT/src/components/SignUp/EndForm/index.jsx
@@ -6,7 +6,10 @@ import './end-form.scss';
 const EndForm = () => (
   <div className="signup end">
     <h2>Fin</h2>
-    <button type="button" className="dog__input-infos__submit">Chercher une balade</button>
+    <p>
+      Ca y est, tout est prêt ! Tu peux enfin trouver les balades à côté de chez toi :smiley:
+    </p>
+    <button type="button" className="end__submit">Chercher une balade</button>
   </div>
 );
 
--- FRONT/src/components/SignUp/UserForm/index.jsx
+++ FRONT/src/components/SignUp/UserForm/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
-// required les champs
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { useForm } from 'react-hook-form';
 import { useSelector } from 'react-redux';
@@ -18,57 +18,78 @@ const UserForm = () => {
 
   return (
     <div className="signup user-form">
-      <div className={isFormComplete ? 'part-bullet part-bullet_complete' : 'part-bullet'}>{isFormComplete ? '✓' : ''}</div>
-      <h2 className={isFormComplete ? 'title-valid' : ''}>Vous</h2>
-      {/* div for display hidden */}
-      <div className={isFormComplete ? 'signup-hide' : ''}>
+      {/* <div className={isFormComplete ? 'part-bullet part-bullet_complete' : 'part-bullet'}>{isFormComplete ? '✓' : ''}</div> */}
+      <h2 className="signup__subtitle">Vous</h2>
+
+      <div className="user-form__form"> {/* isFormComplete ? 'signup-hide' : '' */}
+
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="signup__input-infos">
+          <div className="user-form__form__input-infos">
+            <div className="user-form__form__input-infos__identity">
+              {/* Email */}
+              <div className="user-form__form__input-infos__email">
+                <label htmlFor="email">Votre adresse e-mail</label>
+                <input {...register('email', { required: true })} type="email" id="email" value="email" placeholder="Email" />
+              </div>
 
-            {/* Email */}
-            <div className="signup__input-infos__email">
-              <label htmlFor="email">Votre adresse e-mail</label>
-              <input {...register('email')} type="email" id="email" value="email" placeholder="Email" />
-            </div>
+              {/* Password */}
+              <div className="user-form__form__input-infos__password">
+                <label htmlFor="password">Votre mot de passe</label>
+                <input {...register('password', { required: true })} type="password" id="password" value="password" placeholder="Mot de passe" />
+              </div>
 
-            {/* Password */}
-            <div className="signup__input-infos__password">
-              <label htmlFor="password">Votre mot de passe</label>
-              <input {...register('password')} type="password" id="password" value="password" placeholder="Mot de passe" />
-            </div>
+              {/* Password confirmation */}
+              <div className="user-form__form__input-infos__password--confirmation">
+                <label htmlFor="password-confirmation">Confirmer votre mot de passe</label>
+                <input {...register('password-confirmation', { required: true })} type="password" id="password-confirmation" value="password-confirmation" placeholder="Confirmer mot de passe" />
+              </div>
 
-            {/* Password confirmation */}
-            <div className="signup__input-infos__password--confirmation">
-              <label htmlFor="password-confirmation">Confirmer votre mot de passe</label>
-              <input {...register('password-confirmation')} type="password" id="password-confirmation" value="password-confirmation" placeholder="Confirmer mot de passe" />
-            </div>
+              {/* Firstname */}
+              <div className="user-form__form__input-infos__firstname">
+                <label htmlFor="firstname">Votre Prénom</label>
+                <input {...register('firstname', { required: true })} type="text" id="firstname" value="firstname" placeholder="Prénom" />
+              </div>
 
-            {/* Firstname */}
-            <div className="signup__input-infos__firstname">
-              <label htmlFor="firstname">Votre Prénom</label>
-              <input {...register('firstname')} type="text" id="firstname" value="firstname" placeholder="Prénom" />
-            </div>
+              {/* Name */}
+              <div className="user-form__form__input-infos__name">
+                <label htmlFor="name">Votre Nom</label>
+                <input {...register('name', { required: true })} type="text" id="name" value="name" placeholder="Nom" />
+              </div>
 
-            {/* Name */}
-            <div className="signup__input-infos__name">
-              <label htmlFor="name">Votre Nom</label>
-              <input {...register('name')} type="text" id="name" value="name" placeholder="Nom" />
+              {/* Picture profile */}
+              <div className="user-form__form__input-infos__picture">
+                <label htmlFor="user-picture">Ajouter une photo de profil</label>
+                <input
+                  {...register(('user-picture'))}
+                  type="file"
+                  id="user-picture"
+                  name="user-picture"
+                  accept="image/png, image/jpeg"
+                />
+              </div>
             </div>
 
             {/* Zipcode */}
-            <div className="signup__input-infos__zipcode">
-              <p>Afin de pouvoir géolocaliser votre ville, nous avons besoin de votre code postal.</p>
-              <label htmlFor="zipcode">Votre code postal</label>
-              <input {...register('zipcode')} type="number" id="zipcode" value="zipcode" placeholder="Code postal" />
+            <div className="user-form__form__input-infos__zipcode">
+              <p>
+                Afin de pouvoir géolocaliser votre ville,
+                nous avons besoin de votre code postal.
+              </p>
+              <div className="user-form__form__input-infos__zipcode__input">
+                <label htmlFor="zipcode">Votre code postal</label>
+                <input {...register('zipcode', { required: true })} type="number" id="zipcode" value="zipcode" placeholder="Code postal" />
+              </div>
             </div>
-
-            <div className="signup__input-infos__picture">
-              <p>Ajouter une photo de profil</p>
-            </div>
-
           </div>
-          <button type="submit" className="dog__input-infos__back">Retour</button>
-          <button type="submit" className="dog__input-infos__submit">Continuer</button>
+          <div className="signup__back-submit">
+            <Link to="/" exact="true" className="signup__back-submit__back">Retour</Link>
+            <button
+              type="submit"
+              className="signup__back-submit__submit"
+            >Continuer
+              {/* click = change état à true, isClicked ? et true = mets hidden à la div et passe dog en false */}
+            </button>
+          </div>
         </form>
       </div>
     </div>
