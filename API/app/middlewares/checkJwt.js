78b1e1f022214e2a2181f44adcd16463c9c5ce69
collet julien <collet.jln@gmail.com>
2021-09-23 14:14:36 +0200

    service token

---
+++ API/app/middlewares/checkJwt.js
@@ -0,0 +1,17 @@
+const jwt = require('../services/jwtoken');
+
+
+async function checkToken (request, response, next){
+    try {
+        const token = request.body.token;
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+
+
+
