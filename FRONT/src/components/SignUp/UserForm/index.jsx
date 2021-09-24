5d7ec8aaa37aec1cad9be6f995ee8125ac34ad3e
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-09-24 23:08:49 +0200

    :art: UserForm : improve CSS and remove labels

--- FRONT/src/components/SignUp/UserForm/index.jsx
+++ FRONT/src/components/SignUp/UserForm/index.jsx
@@ -36,31 +36,31 @@ const UserForm = () => {
           <div className="user-form__form__input-infos__identity">
             {/* Email */}
             <div className="user-form__form__input-infos__email">
-              <label htmlFor="email">Votre adresse e-mail</label>
+              {/* <label htmlFor="email">Votre adresse e-mail</label> */}
               <input {...register('email', { required: 'Veuillez entrer un email' })} type="email" id="email" name="email" placeholder="Email" />
             </div>
 
             {/* Password */}
             <div className="user-form__form__input-infos__password">
-              <label htmlFor="password">Votre mot de passe</label>
+              {/* <label htmlFor="password">Votre mot de passe</label> */}
               <input {...register('password', { required: 'Veuillez entrer un mot de passe' })} type="password" id="password" name="password" placeholder="Mot de passe" />
             </div>
 
             {/* Password confirmation */}
             <div className="user-form__form__input-infos__password--confirmation">
-              <label htmlFor="password_confirmation">Confirmer votre mot de passe</label>
+              {/* <label htmlFor="password_confirmation">Confirmer votre mot de passe</label> */}
               <input {...register('password_confirmation', { required: 'Veuillez confirmer votre mot de passe' })} type="password" id="password_confirmation" name="password_confirmation" placeholder="Confirmer mot de passe" />
             </div>
 
             {/* Firstname */}
             <div className="user-form__form__input-infos__first_name">
-              <label htmlFor="first_name">Votre Prénom</label>
+              {/* <label htmlFor="first_name">Votre Prénom</label> */}
               <input {...register('first_name', { required: 'Veuillez entrer votre prénom' })} type="text" id="first_name" name="first_name" placeholder="Prénom" />
             </div>
 
             {/* Name */}
             <div className="user-form__form__input-infos__name">
-              <label htmlFor="last_name">Votre Nom</label>
+              {/* <label htmlFor="last_name">Votre Nom</label> */}
               <input {...register('last_name', { required: 'Veuillez entrer votre nom' })} type="text" id="last_name" name="last_name" placeholder="Nom" />
             </div>
 
@@ -84,7 +84,7 @@ const UserForm = () => {
               nous avons besoin de votre code postal.
             </p>
             <div className="user-form__form__input-infos__zipcode__input">
-              <label htmlFor="zip_code">Votre code postal</label>
+              {/* <label htmlFor="zip_code">Votre code postal</label> */}
               <input {...register('zip_code', { required: 'Veuillez entrer votre code postal' })} type="number" id="zip_code" name="zip_code" placeholder="Code postal" />
             </div>
           </div>
