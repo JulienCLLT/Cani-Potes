fa081021e763672f827e2a93c6e95eb7190b6fc3
Alekiel42 <alexandra.bercoff@gmail.com>
2021-09-24 11:45:25 +0200

    update gitgnore

--- API/app/middlewares/checkJwt.js
+++ API/app/middlewares/checkJwt.js
@@ -4,7 +4,6 @@ const jwt = require('../services/jwtoken');
 module.exports =(request, response, next)=>{
     try {
         let token = request.get('authorization');
-        console.log(token);
         
         const verify =jwt.verifyToken(token);
         //request.userId objet pour le back et les requetes sql
