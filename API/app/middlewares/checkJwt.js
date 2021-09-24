c304f9ca0dca96f4c4aa9fa549d85a1cdb864260
Alekiel42 <alexandra.bercoff@gmail.com>
2021-09-24 11:42:21 +0200

    add findOne User and test route

--- API/app/middlewares/checkJwt.js
+++ API/app/middlewares/checkJwt.js
@@ -4,6 +4,7 @@ const jwt = require('../services/jwtoken');
 module.exports =(request, response, next)=>{
     try {
         let token = request.get('authorization');
+        console.log(token);
         
         const verify =jwt.verifyToken(token);
         //request.userId objet pour le back et les requetes sql
