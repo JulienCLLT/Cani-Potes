ea9e33bc40a153d95927ee85c61e28e4835d0575
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-09-24 15:37:18 +0200

    :construction: pb with states and form, put form in parent component

--- FRONT/src/components/SignUp/index.jsx
+++ FRONT/src/components/SignUp/index.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable linebreak-style */
 
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useSelector, useDispatch } from 'react-redux';
 
 // import component
 import DogForm from './DogForm/index';
@@ -10,14 +12,30 @@ import Timeline from './Timeline/index';
 
 import './signup.scss';
 
-const SignUp = () => (
-  <div className="signup-main">
-    <h1>Inscription</h1>
-    <Timeline />
-    <UserForm />
-    <DogForm />
-    <EndForm />
-  </div>
-);
+const SignUp = () => {
+  const { handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="signup-main">
+      <h1>Inscription</h1>
+      <Timeline />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* {formStep === 1 && <UserForm /> }
+        {formStep === 2 && <DogForm /> }
+        {formStep === 3 && <EndForm /> }
+        {formStep === 3 && (
+        <button type="submit" className="end__submit signup-main__submit">Chercher une balade</button>
+        )} */}
+        <UserForm />
+        <DogForm />
+        <EndForm />
+        <button type="submit" className="end__submit signup-main__submit">Chercher une balade</button>
+      </form>
+    </div>
+  );
+};
 
 export default SignUp;
