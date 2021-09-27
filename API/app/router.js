d20674059774632156249f650bdb0600eea24981
Alekiel42 <alexandra.bercoff@gmail.com>
2021-09-27 13:51:09 +0200

    Sent characteristic dog : breed and behavior ok

--- API/app/router.js
+++ API/app/router.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 const router = Router();
 const userController = require('./controllers/userController');
-const ckeckToken  = require('./middlewares/checkJwt');
+const checkToken  = require('./middlewares/checkJwt');
 const rideController = require('./controllers/rideController');
 const dogController = require('./controllers/dogController');
+const formController = require('./controllers/formController');
 
 const multer  = require('multer');
 const storage = multer.diskStorage({
@@ -11,8 +12,10 @@ const storage = multer.diskStorage({
 })
 const upload = multer({ storage: storage });
 
-router.get('/', ckeckToken, function (req, res) {
+router.get('/', checkToken, function (req, res) {
     res.send('GET request to the homepage token ok '+ req.userId)});
+
+router.get('/characteristic', formController.getDogCharacteristic);
 
 router.post('/login',userController.login),
 
@@ -20,15 +23,16 @@ router.post('/subscribe',userController.addNewUser);
 
 router.get('/rides', rideController.findAll);
 
-router.delete('/ride/:rideId(\\d+)', ckeckToken, rideController.delete);
+router.delete('/ride/:rideId(\\d+)', checkToken, rideController.delete);
 //todo quand front ok, rajout checkToken
 router.get('/ride/:rideId(\\d+)', rideController.findOneRideWithAllInformations);
 
-router.delete('/ride/:rideId(\\d+)/participation', ckeckToken, rideController.leaveARide);
-router.post('/ride/:rideId(\\d+)/participation', ckeckToken, rideController.addNewParticipant);
+router.delete('/ride/:rideId(\\d+)/participation', checkToken, rideController.leaveARide);
+router.post('/ride/:rideId(\\d+)/participation', checkToken, rideController.addNewParticipant);
 
-router.delete('/ride/:rideId(\\d+)/participation/user/:userId(\\d+)', ckeckToken, rideController.removeUserFromRide);
+router.delete('/ride/:rideId(\\d+)/participation/user/:userId(\\d+)', checkToken, rideController.removeUserFromRide);
 
+//todo checktoken
 router.get('/profile/:profileId(\\d+)/dogs/:dogId(\\d+)', checkToken, dogController.getOneDog);
 
 module.exports = router;
