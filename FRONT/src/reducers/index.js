f0557f958b44ccc88d7a013defdb4767aa7f5bc9
Raphael Charousset <raphael.charousset@gmail.com>
2021-09-27 21:30:25 +0200

    profile other user without style ok

--- FRONT/src/reducers/index.js
+++ FRONT/src/reducers/index.js
@@ -4,11 +4,13 @@ import { combineReducers } from 'redux';
 import ridesReducer from './ridesReducer';
 import userReducer from './userReducer';
 import signupReducer from './signupReducer';
+import profileReducer from './profileReducer';
 
 const rootReducer = combineReducers({
   rides: ridesReducer,
   user: userReducer,
   signup: signupReducer,
+  profile: profileReducer,
 });
 
 export default rootReducer;
