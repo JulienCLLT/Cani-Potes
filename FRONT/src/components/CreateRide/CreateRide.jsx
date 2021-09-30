c670d110b023891888d67dcf6e55b28aacdd6115
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-09-30 20:22:47 +0200

    :construction: finished form create ride and mw

--- FRONT/src/components/CreateRide/CreateRide.jsx
+++ FRONT/src/components/CreateRide/CreateRide.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // import leaflet
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import {
+  MapContainer, TileLayer, Marker, useMapEvents,
+} from 'react-leaflet';
 import L from 'leaflet';
+import { createRide } from '../../actions/rides';
 
 import './createRide.scss';
 
@@ -13,11 +16,15 @@ import startPointFlag from '../../assets/img/info-ride/startPointFlag.svg';
 import endPointFlag from '../../assets/img/info-ride/endPointFlag.svg';
 
 const CreateRide = () => {
+  const { failedToCreateRide } = useSelector((state) => state.rides);
+  const { errorMessage } = useSelector((state) => state.rides);
   const { user } = useSelector((state) => state);
   const { register, handleSubmit, formState: { errors } } = useForm();
 
+  const dispatch = useDispatch();
   const onSubmit = (data) => {
     // dispatch action to post data to bdd through middleware and if ride is created, add it to state
+    dispatch(createRide(data));
     console.log('submitted data : ', data);
   };
 
@@ -67,29 +74,33 @@ const CreateRide = () => {
     <main className="create-ride">
       <h2>Création d'une balade</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
+        {
+          failedToCreateRide && <span>{errorMessage}</span>
+        }
 
         <div className="create-ride__field">
           <label htmlFor="title">Nom de ma balade</label>
           <input
+            type="text"
             id="title"
             name="title"
             defaultValue="Ma super balade"
-            {...register('title', { required: 'Title is needed' })}
+            {...register('title', { required: 'Veuillez écrire un titre.', maxLength: { value: 20, message: 'Veuillez ne pas dépasser 20 caractères.' } })}
           />
-          {errors.title && <span>Le titre est obligatoire</span>}
+          {errors.title && <span>{errors.title.message}</span>}
         </div>
 
         <div className="create-ride__field__map-container">
           <div className="create-ride__field__map-points">
             <button
-              className={switchPoint === 'start' && 'selected'}
+              className={switchPoint === 'start' ? 'selected' : ''}
               type="button"
               onClick={() => setSwitchPoint('start')}
             >
               Départ
             </button>
             <button
-              className={switchPoint === 'end' && 'selected'}
+              className={switchPoint === 'end' ? 'selected' : ''}
               type="button"
               onClick={() => setSwitchPoint('end')}
             >
@@ -120,27 +131,35 @@ const CreateRide = () => {
             type="date"
             defaultValue={`${date.getUTCFullYear().toString().padStart(2, '0')}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${(date.getUTCDate() + 1).toString().padStart(2, '0')}`}
             min={`${date.getUTCFullYear().toString().padStart(2, '0')}-${(date.getUTCMonth() + 1).toString().padStart(2, '0')}-${date.getUTCDate().toString().padStart(2, '0')}`}
-            {...register('date', { required: 'A day is needed' })}
+            {...register('date', { required: 'Veuillez sélectionner la date de la balade.' })}
           />
-          {errors.date && <span>Le choix d'une date est obligatoire</span>}
+          {errors.date && <span>{errors.date.message}</span>}
         </div>
 
         <div className="create-ride__field">
           <p>Heure de départ</p>
           <label htmlFor="startHour" />
-          <select {...register('startHour')} defaultValue={17}>
+          <select {...register('startHour', { required: 'Veuillez sélectionner l\'heure de la balade.' })} defaultValue={17}>
             {
-							hours.map((hour) => <option key={hour} value={hour}>{hour.toString().padStart(2, '0')}h</option>)
+							hours.map((hour) => <option key={hour} value={hour}>{hour.toString().padStart(2, '0')}</option>)
 						}
           </select>
           <label htmlFor="startMin" />
-          <select {...register('startMin')} defaultValue={30}>
+          <select {...register('startMin', { required: 'Veuillez sélectionner les minutes de l\'heure de la balade.' })} defaultValue={30}>
             {
 							minutes.map((minute) => <option key={minute} value={minute}>{minute.toString().padStart(2, '0')}</option>)
 						}
           </select>
+          {/* je convertis en number puis je fais si x<minHour alors erreur */}
+          {/* {errors.startHour || errors.startMin && <span>{errors.startHour.message}</span> } */}
 
-          {errors.startHour || errors.startMin && <span>L'heure de départ est obligatoire</span>}
+          {errors.startHour || errors.startMin && (
+          <>
+            <span>{errors.startHour.message}</span>
+            <span>{errors.starMin.message}</span>
+          </>
+          )}
+          {errors.startHour && <span>{errors.startHour.message}</span>}
         </div>
 
         <div className="create-ride__field">
@@ -148,21 +167,22 @@ const CreateRide = () => {
           <input
             id="maxDogs"
             name="maxDogs"
-            min={2}
             defaultValue={4}
             type="number"
-            {...register('maxDogs', { required: 'maxDogs is needed' })}
+            {...register('maxDogs', {
+              required: 'Veuillez remplir le nombre maximum de chiens', max: { value: 5, message: 'Maximum 5 chiens' }, min: { value: 2, message: 'Minimum 2 chiens' },
+            })}
           />
-          {errors.maxDogs && <span>Le nombre max de chiens est obligatoire</span>}
+          {errors.maxDogs && <span>{errors.maxDogs.message}</span>}
         </div>
 
         <div className="create-ride__field">
           <label htmlFor="description">Description de ma balade</label>
           <textarea
             placeholder="Je souhaite me faire des Cani Potes :)"
-            {...register('description', { required: 'Description is needed' })}
+            {...register('description', { required: 'Veuillez remplir la description.', maxLength: { value: 200, message: 'Veuillez ne pas dépasser 200 caractères.' } })}
           />
-          {errors.description && <span>La description est obligatoire</span>}
+          {errors.description && <span>{errors.description.message}</span>}
         </div>
 
         <input type="submit" />
