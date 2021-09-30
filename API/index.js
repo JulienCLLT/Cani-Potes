b0532e1566d73cd5d1f63189b30fb92a914e3b93
Alekiel42 <alexandra.bercoff@gmail.com>
2021-09-30 17:47:42 +0200

    change express.static name : public to images

--- API/index.js
+++ API/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static('images'));
 
 app.use('/api', route);
 
