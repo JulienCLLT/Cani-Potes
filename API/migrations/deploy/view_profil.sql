0a890b1459bf1b0f654bee0b71636b8ecf7e1736
collet julien <collet.jln@gmail.com>
2021-09-30 13:38:56 +0200

    :construction: view profile no finish

--- API/migrations/deploy/view_profil.sql
+++ API/migrations/deploy/view_profil.sql
@@ -2,7 +2,7 @@
 
 BEGIN;
 
-SELECT  member.id,
+SELECT  member.id as member_id,
 		member.email,
 		member.first_name,
 		member.last_name,
@@ -10,22 +10,32 @@ SELECT  member.id,
 		member.zip_code,
 		member.birthday,
  
-			array_agg(jsonb_build_object(
+			array_agg(distinct jsonb_build_object(
 					 'dog_id', dog.id,
 					 'dog_surname', dog.surname,
 					 'dog_behavior', behavior.label,
 					 'dog_breed', breed.label,
 					 'dog_gender', gender.label,
 					 'dog_weight', dog.weight,
-					 'dog_age', dog.birthday,
+					 'dog_age', CASE 
+                                       WHEN ((NOW()::DATE - dog.birthday)/365) < 1 THEN ((NOW()::DATE - dog.birthday)/30)::varchar || ' mois' 
+                                       WHEN ((NOW()::DATE - dog.birthday)/365) = 1 THEN ((NOW()::DATE - dog.birthday)/30)::varchar || ' an'
+                                       ELSE ((NOW()::DATE - dog.birthday)/365)::varchar || ' ans'
+                               END ,
 					 'dog_sterilization', dog.sterilization,
-					 'dog_description', dog.description
+					 'dog_description', dog.description,
+					 'dog_photo', jsonb_build_object(
+												'photo_id', photo.id,
+												'photo_url', photo.file_name)
+							
 					))filter (where dog_owner_id is not null)as dogs
-FROM member
+FROM member 
 left join dog on dog_owner_id = member.id
 left join behavior on dog.behavior_id = behavior.id
 left join breed on dog.breed_id = breed.id
 left join gender on dog.gender_id = gender.id
+left join photo on photo.dog_id = dog.id
+
 group by member.id
 
 
