2bbf9549e5630a4e9b1204b06b493545da204fa7
Alekiel42 <alexandra.bercoff@gmail.com>
2021-09-30 17:45:28 +0200

    resize photo with sharp OK

--- API/app/controllers/dogController.js
+++ API/app/controllers/dogController.js
@@ -1,5 +1,8 @@
 const Dog = require('../models/dogModel');
 const Photo = require('../models/photoModel');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 const dogController = {
     
@@ -36,29 +39,32 @@ const dogController = {
                 throw Error('La valeur de l\'id doit être un nombre');
             }
 
-            const userId = request.userId;
-            if(userId !== profileId ){
-                throw Error('Vous ne pouvez pas ajouter de chien à ce profil');
-            }
+            // const userId = request.userId;
+            // if(userId !== profileId ){
+            //     throw Error('Vous ne pouvez pas ajouter de chien à ce profil');
+            // }
 
             const newDog = new Dog(request.body);
             const dogCreated = await newDog.create();
 
             if(request.file) {
-                console.log("photo");
+                const { filename: image } = request.file;
+
+                // resize picture and push it in resized file
+                await sharp(request.file.path).resize(200, 200).jpeg({ quality: 90 })
+                .toFile(path.resolve(request.file.destination,'resized',image));
+                fs.unlinkSync(request.file.path);
+
+                // insert the photo data in db
                 const newPhoto = new Photo({ file : request.file.filename, dogId : dogCreated.id}); 
                 console.log("new phoho", newPhoto);
                 const photoCreated = await newPhoto.addPhoto();
-                console.log(photoCreated);
                 dogCreated.photo = photoCreated;
             }
-
             response.status(201).json(dogCreated);
-
         } catch (error) {
             response.status(500).json(error.message);
         }
     }
 };
-
 module.exports = dogController;
--- API/app/middlewares/multerConfig.js
+++ API/app/middlewares/multerConfig.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
-  filename: (req, file, callback) => {
+  filename: async (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
