1917fee027acbd3d05f79e33c9caf142f6e0d58f
mathilde-chenillot <mathilde.cheni@gmail.com>
2021-10-01 14:31:57 +0200

    :bug: fix mw bug

--- FRONT/src/components/CreateRide/CreateRide.jsx
+++ FRONT/src/components/CreateRide/CreateRide.jsx
@@ -46,7 +46,6 @@ const CreateRide = () => {
   const [switchPoint, setSwitchPoint] = useState('start');
   const [startPoint, setStartPoint] = useState(user.position);
   const [endPoint, setEndPoint] = useState();
-  console.log(endPoint);
 
   const onSubmit = (data) => {
     console.log(data);
