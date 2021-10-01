a05ae550514d42571295399fdafd225d7c61dd84
Raphael Charousset <raphael.charousset@gmail.com>
2021-10-01 23:05:40 +0200

    added dog behavior icons dynamicly

--- FRONT/src/components/RideDetails/RideDetails.jsx
+++ FRONT/src/components/RideDetails/RideDetails.jsx
@@ -14,8 +14,12 @@ import calendar from '../../assets/img/info-ride/calendar.svg';
 import hourglass from '../../assets/img/info-ride/hourglass.svg';
 import startFlag from '../../assets/img/info-ride/startPointFlag.svg';
 import endFlag from '../../assets/img/info-ride/endPointFlag.svg';
-import doubleArrow from '../../assets/img/info-ride/double_arrow.svg';
+import conversation from '../../assets/img/info-ride/conversation.svg';
 import close from '../../assets/img/close.svg';
+import peureux from '../../assets/img/profile-simulation/fearful.svg';
+import joueur from '../../assets/img/profile-simulation/player.png';
+import agressif from '../../assets/img/profile-simulation/aggressive.png';
+import sociable from '../../assets/img/profile-simulation/sociable.svg';
 
 import './RideDetails.scss';
 import {
@@ -44,6 +48,20 @@ const RideDetails = () => {
   let nbOfDogs = 0;
 
   participants.map((participant) => nbOfDogs += participant.dogs.length);
+
+  participants.sort((a, b) => {
+    if (a.participant_id === host_id) {
+      return -1;
+    }
+    return 0;
+  });
+
+  const dogBehaviors = {
+    peureux,
+    joueur,
+    agressif,
+    sociable,
+  };
 
   const { register, handleSubmit, reset } = useForm();
 
@@ -132,7 +150,7 @@ const RideDetails = () => {
               isLoading ? (
                 <span>chargement ...</span>
               ) : (
-                <MapContainer className="ride-details__leaflet__map" center={start_coordinate} zoom={16} scrollWheelZoom>
+                <MapContainer className="ride-details__leaflet__map" center={start_coordinate} zoom={14} scrollWheelZoom>
                   <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                   <Marker position={start_coordinate} icon={positionStart}>
                     <Popup>Départ</Popup>
@@ -213,8 +231,12 @@ const RideDetails = () => {
                     if (index < 3) {
                       return (
                         <div className="ride-details__current-user__dogs" key={`${dog.dog_id}`}>
-                          <img src={dog.dog_photo[0]} alt="dog" />
+                          <img src={dog.dog_photo[0]} alt={dog.dog_surname} />
                           <span>{dog.dog_surname}</span>
+                          <span>
+                            <img src={dogBehaviors[dog.dog_behavior]} alt="" />
+                            {dog.dog_behavior}
+                          </span>
                         </div>
                       );
                     }
@@ -254,7 +276,13 @@ const RideDetails = () => {
           className={isChatOpen ? 'ride-details__toggle rotate' : 'ride-details__toggle'}
           onClick={() => setIsChatOpen(!isChatOpen)}
         >
-          <img src={doubleArrow} alt="arrow" />
+          {
+            isChatOpen ? (
+              <img src={close} alt="close chat" />
+            ) : (
+              <img src={conversation} alt="open chat" />
+            )
+          }
         </button>
         )
       }
