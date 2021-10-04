bc1c3dc50452376802ef4e5f0463c97ea13bb8cd
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-04 10:49:50 +0200

    update sql query on findRidesByMember

--- API/app/models/rideModel.js
+++ API/app/models/rideModel.js
@@ -45,10 +45,24 @@ class Ride {
 
     static async findRidesByMember(userId) {
         try {
+            //todo facto
             const query = `
-                SELECT ride.id AS ride_id, host_id, mp.member_id AS participant FROM ride 
+                SELECT 
+                    ride.id AS ride_id, 
+                    host_id, 
+                    title,
+                    to_char(starting_time, 'TMDay DD TMMonth YYYY "à" HH "h" MI') AS starting_time,
+                    max_number_dogs,
+                    ARRAY_AGG(DISTINCT
+                                jsonb_build_object(
+                                    'participant_id', mp.member_id,
+                                    'dogs', (SELECT ARRAY_AGG(dog.id) FROM dog WHERE dog_owner_id = mp.member_id)
+                                    )
+                            ) AS participants
+                FROM ride 
                 JOIN member_participate_ride AS mp ON ride.id = mp.ride_id
-                WHERE ride.host_id = $1 OR mp.member_id = $1           
+                WHERE ride.host_id = $1 OR mp.member_id = $1
+                GROUP BY ride.id, host_id, title, starting_time, max_number_dogs     
             `;
             const { rows } = await client.query(query, [userId]);
             return rows.map(row => new Ride(row));
