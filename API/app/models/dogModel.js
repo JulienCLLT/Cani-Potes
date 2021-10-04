8ce7d605db8b27b6baec8762d73103e269becf96
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-04 09:26:22 +0200

    add dogModel method

--- API/app/models/dogModel.js
+++ API/app/models/dogModel.js
@@ -1,4 +1,4 @@
-const client = require('./../database');
+const client = require("./../database");
 
 class Dog {
     constructor(data = {}) {
@@ -21,18 +21,36 @@ class Dog {
         }
     }
 
-    static async delete(id) {
+    async create() {
         try {
-            const query = `DELETE FROM dog WHERE id = $1`;
-            await client.query(query, [id]);
-            return null;
+            //todo faire function sql
+            // verif si prenom existe deja dans BDD avec cet id et ce surname
+            const query = `INSERT INTO dog(surname, description, weight, birthday, sterilization, breed_id, gender_id, behavior_id, dog_owner_id) 
+             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`;
+
+            const { rows } = await client.query(query, [
+                this.surname,
+                this.description,
+                parseFloat(this.weight),
+                this.birthday,
+                this.sterilization,
+                Number(this.breed_id),
+                Number(this.gender_id),
+                Number(this.behavior_id),
+                Number(this.dog_owner_id),
+            ]);
+
+            this.id = rows[0].id;
+            return rows[0];
         } catch (error) {
             console.error(error);
             throw new Error(error.detail ? error.detail : error.message);
         }
     }
 
+
 }
 
 module.exports = Dog;
+
 
