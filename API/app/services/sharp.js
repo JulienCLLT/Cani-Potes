5bdcea9790652efda6c247453d7c2df1747c78b8
Alekiel42 <alexandra.bercoff@gmail.com>
2021-10-07 14:44:12 +0200

    init commit

--- API/app/services/sharp.js
+++ API/app/services/sharp.js
@@ -3,27 +3,22 @@ const path = require('path');
 const fs = require('fs');
 
 const sharpResizeImage = {
-
-    async sharpResize (request,nameFolderStore){
+    async sharpResize(request, nameFolderStore) {
         try {
             await sharp(request.path).resize(200, 200)
-            .jpeg({ quality: 90 })
-            .toFile(path.resolve(
-            request.destination, `${nameFolderStore}`, request.filename
-            ));
+                .jpeg({ quality: 90 })
+                .toFile(path.resolve(
+                    request.destination, `${nameFolderStore}`, request.filename
+                ));
 
             fs.unlinkSync(request.path);
         } catch (error) {
             throw error;
         }
-        
-
-        
     },
 
-     delOldImage: (nameFolderStore ,nameOldImage,) => {
-
-         fs.unlinkSync(`public/${nameFolderStore}/${nameOldImage}`);
+    delOldImage: (nameFolderStore, nameOldImage,) => {
+        fs.unlinkSync(`public/${nameFolderStore}/${nameOldImage}`);
     }
 
 };
