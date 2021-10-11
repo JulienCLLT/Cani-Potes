821dd4ca4f447903572bb2653d25a49c76192188
Raphael Charousset <raphael.charousset@gmail.com>
2021-10-11 16:59:45 +0200

    attempt to style profile

--- FRONT/src/components/Profile/index.jsx
+++ FRONT/src/components/Profile/index.jsx
@@ -167,7 +167,7 @@ const Profile = () => {
         profile.isLoading ? (
           <Loader />
         ) : (
-          <>
+          <div className="profile-page__content-wrapper">
             <header className="profile-page__header">
               <div className="profile-page__header-container">
                 <div className="profile-page__header__avatar">
@@ -194,23 +194,72 @@ const Profile = () => {
                       <span className="profile-page__header__user__zipcode">{profile.zip_code}</span>
                     )
                   }
+                  {
+                    profileIsUser && (
+                      <div
+                        className="profile-page__header__edit  edit-btn"
+                        onClick={toggleEditUser}
+                      >
+                        {isEditingUser ? 'Retour' : (
+                          <>
+                            <img src={edit} alt="edit" />
+                            Modifier
+                          </>
+                        )}
+                      </div>
+                    )
+                  }
                 </div>
-                {
-                  profileIsUser && (
-                    <div
-                      className="profile-page__header__edit  edit-btn"
-                      onClick={toggleEditUser}
-                    >
-                      {isEditingUser ? 'Retour' : (
-                        <>
-                          <img src={edit} alt="edit" />
-                          Modifier
-                        </>
-                      )}
-                    </div>
-                  )
-                }
               </div>
+              <section className="profile-page__info-user">
+                {profileIsUser && isEditingUser && (
+                  <button
+                    className="profile-page__info-user__delete-account-btn"
+                    type="button"
+                    onClick={() => {
+                      setInputDelete('');
+                      setFailedToDelete(false);
+                      setIsModalAccountOpen(true);
+                    }}
+                  >
+                    Supprimer mon compte
+                  </button>
+                )}
+                { isEditingUser && (
+                  <p className="profile-page__info-user__content">
+                    <span className="profile-page__info-user__content-field">
+                      {
+                      isEditingUser && (
+                        <input
+                          type="text"
+                          value={firstName}
+                          onChange={(e) => setFirstName(e.target.value)}
+                        />
+                      )
+                    }
+                      {
+                      isEditingUser && (
+                        <input
+                          type="text"
+                          value={lastName}
+                          onChange={(e) => setLastName(e.target.value)}
+                        />
+                      )
+                    }
+                    </span>
+                  </p>
+                )}
+                {isEditingUser && (
+                  <div className="profile-page__info-user__submit">
+                    <button
+                      type="submit"
+                      onClick={handleUpdateUser}
+                    >
+                      Enregistrer vos infos
+                    </button>
+                  </div>
+                )}
+              </section>
             </header>
 
             {isModalDeleteAccountOpen && (
@@ -223,56 +272,6 @@ const Profile = () => {
                 firstName={user.first_name}
               />
             )}
-
-            <section className="profile-page__info-user">
-              {profileIsUser && isEditingUser && (
-                <button
-                  className="profile-page__info-user__delete-account-btn"
-                  type="button"
-                  onClick={() => {
-                    setInputDelete('');
-                    setFailedToDelete(false);
-                    setIsModalAccountOpen(true);
-                  }}
-                >
-                  Supprimer mon compte
-                </button>
-              )}
-              { isEditingUser && (
-                <p className="profile-page__info-user__content">
-                  <span className="profile-page__info-user__content-field">
-                    {
-                    isEditingUser && (
-                      <input
-                        type="text"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                      />
-                    )
-                  }
-                    {
-                    isEditingUser && (
-                      <input
-                        type="text"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
-                      />
-                    )
-                  }
-                  </span>
-                </p>
-              )}
-              {isEditingUser && (
-                <div className="profile-page__info-user__submit">
-                  <button
-                    type="submit"
-                    onClick={handleUpdateUser}
-                  >
-                    Enregistrer vos infos
-                  </button>
-                </div>
-              )}
-            </section>
 
             <section className="profile-page__info-dogs">
               {
@@ -312,7 +311,7 @@ const Profile = () => {
                 ))
               }
             </section>
-          </>
+          </div>
         )
       }
 
